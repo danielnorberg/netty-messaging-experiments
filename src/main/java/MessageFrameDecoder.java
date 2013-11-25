f158ee53e16e7fac6431212b9d658d4fbde9e70a
Daniel Norberg <dano@spotify.com>
2013-11-24 22:35:24 -0500

    slice frames

--- src/main/java/MessageFrameDecoder.java
+++ src/main/java/MessageFrameDecoder.java
@@ -1,8 +1,15 @@
+import org.jboss.netty.buffer.ChannelBuffer;
 import org.jboss.netty.handler.codec.frame.LengthFieldBasedFrameDecoder;
 
 public class MessageFrameDecoder extends LengthFieldBasedFrameDecoder {
 
   public MessageFrameDecoder() {
     super(128 * 1024 * 1024, 0, 4, 0, 4);
+  }
+
+  @Override
+  protected ChannelBuffer extractFrame(final ChannelBuffer buffer, final int index,
+                                       final int length) {
+    return buffer.slice(index, length);
   }
 }
