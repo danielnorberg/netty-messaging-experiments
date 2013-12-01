16cd13791e25211f2226823cc0aa25cb8496f697
Daniel Norberg <dano@spotify.com>
2013-11-30 21:26:56 -0500

    Revert "single threaded batching"
    This reverts commit d75c9cc18fc94ac9065fb6d20064d4fa1338fc28.

--- src/main/java/BatchWriter.java
+++ src/main/java/BatchWriter.java
@@ -1,6 +1,6 @@
-import com.google.common.collect.Lists;
-
-import java.util.List;
+import java.util.Deque;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicIntegerFieldUpdater;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.ByteBufAllocator;
@@ -13,16 +13,20 @@ public class BatchWriter {
   private static final long MAX_DELAY_NANOS = MICROSECONDS.toNanos(100);
   private static final int MAX_BUFFER_SIZE = 4096;
 
+  private static final AtomicIntegerFieldUpdater<BatchWriter> bufferSizeUpdater =
+      AtomicIntegerFieldUpdater.newUpdater(BatchWriter.class, "bufferSize");
   private static final int HEADER_SIZE = 4;
 
   private final Channel channel;
   private final ByteBufAllocator allocator;
 
+  private final AtomicBoolean flushing = new AtomicBoolean();
+
   public volatile long p0, p1, p2, p3, p4, p5, p6, p7;
   public volatile long q0, q1, q2, q3, q4, q5, q6, q7;
-  private final List<ByteBuf> queue = Lists.newArrayList();
-  private long lastWriteNanos;
-  private int bufferSize;
+  private volatile long lastWriteNanos;
+  private volatile int bufferSize;
+  private final Deque<ByteBuf> queue = new ConcurrentLinkedDeque<>();
   public volatile long r0, r1, r2, r3, r4, r5, r6, r7;
   public volatile long s0, s1, s2, s3, s4, s5, s6, s7;
 
@@ -39,7 +43,7 @@ public class BatchWriter {
 
     // Calculate new size of outgoing message buffer
     final int frameSize = message.readableBytes() + HEADER_SIZE;
-    bufferSize += frameSize;
+    final int newBufferSize = bufferSizeUpdater.addAndGet(this, frameSize);
 
     // Calculate how long it was since the last outgoing message
     final long now = System.nanoTime();
@@ -48,26 +52,40 @@ public class BatchWriter {
 
     // Flush if writes are sparse or if the buffer has reached its threshold size
     if (nanosSinceLastWrite > MAX_DELAY_NANOS ||
-        bufferSize > MAX_BUFFER_SIZE) {
+        newBufferSize > MAX_BUFFER_SIZE) {
       flush();
     }
   }
 
-  @SuppressWarnings("ForLoopReplaceableByForEach")
   private void flush() {
-    final ByteBuf buffer = allocator.buffer(bufferSize);
+    if (!flushing.compareAndSet(false, true)) {
+      return;
+    }
 
-    for (int i = 0; i < queue.size(); i++) {
-      final ByteBuf message = queue.get(i);
+    // The message buffer will become empty. This is racy but we don't care.
+    final int bufferSize = this.bufferSize;
+    this.bufferSize = 0;
+
+    final ByteBuf buffer = allocator.buffer(bufferSize);
+    long n = 0;
+
+    while (true) {
+      final ByteBuf message = queue.poll();
+      if (message == null) {
+        break;
+      }
       final int size = message.readableBytes();
+      if (n + HEADER_SIZE + size > bufferSize) {
+        queue.addFirst(message);
+        break;
+      }
       buffer.writeInt(size);
       buffer.writeBytes(message);
       message.release();
     }
 
-    bufferSize = 0;
-    queue.clear();
-
     channel.writeAndFlush(buffer);
+
+    flushing.set(false);
   }
 }
