4c2af4ee7361834602d899793fb96b0334640dfb
Daniel Norberg <dano@spotify.com>
2013-12-01 20:54:10 -0500

    request caching hack

--- src/main/java/SimpleBenchReqRep.java
+++ src/main/java/SimpleBenchReqRep.java
@@ -138,8 +138,9 @@ public class SimpleBenchReqRep {
         handlers.remove(this);
       }
 
+      final Request request = new Request(requestIdCounter, buffer(0));
+
       private void send() {
-        final Request request = new Request(requestIdCounter, buffer(0));
         writer.write(request);
         requestIdCounter++;
       }
